08e31a9e64dcfd85dfd2745b21cb3ad176d4e835
jayfar <jasonfarnen@gmail.com>
2015-11-06 15:06:02 -0500

    Initial checkin with autopublish and insecure packages included.

---
+++ polling/client/app.js
@@ -0,0 +1,18 @@
+// adds index to each item
+Template.body.helpers({
+  
+  polls: function() {
+    return Polls.find();
+  }
+  
+});
+
+
+UI.registerHelper('indexedArray', function(context, options) {
+  if (context) {
+    return context.map(function(item, index) {
+      item._index = index;
+      return item;
+    });
+  }
+});
